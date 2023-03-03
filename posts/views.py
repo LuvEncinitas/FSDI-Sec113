5371899a5a434fa91a31d6492af7234e57280a42
LuvEncinitas <luvEncinitas@gmail.com>
2023-03-02 18:37:04 -0800

    final push

--- posts/views.py
+++ posts/views.py
@@ -24,7 +24,11 @@ class PostDetailView(DetailView):
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/new.html"
     model = Post
-    fields =["title", "subtitle", "author", "body", "active"]
+    fields =["title", "subtitle", "body", "active"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
